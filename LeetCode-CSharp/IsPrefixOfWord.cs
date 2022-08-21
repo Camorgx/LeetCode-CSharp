eb788f4a9565fdbb96921963ff37446cb43c34df
星河 <1821365384@qq.com>
2022-08-21 16:05:57 +0800

    新增 1455.

---
+++ LeetCode-CSharp/IsPrefixOfWord.cs
@@ -0,0 +1,23 @@
+﻿namespace IsPrefixOfWord {
+    public static class Test {
+        public static void RunTest() {
+            Solution solution = new();
+            Console.WriteLine(
+                solution.IsPrefixOfWord("i love eating burger", "burg"));
+            Console.WriteLine(
+                solution.IsPrefixOfWord("this problem is an easy problem", "pro")); ;
+            Console.WriteLine(
+                solution.IsPrefixOfWord("i am tired", "you"));
+        }
+    }
+    public class Solution {
+        public int IsPrefixOfWord(string sentence, string searchWord) {
+            var words = sentence.Split();
+            for (int i = 0; i < words.Length; ++i) {
+                if (words[i].StartsWith(searchWord))
+                    return i + 1;
+            }
+            return -1;
+        }
+    }
+}
