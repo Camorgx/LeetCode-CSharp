0ead02489bdcaba9cd32ba0a40d674a4b1b891f0
Bill <1821365384@qq.com>
2022-09-07 08:21:00 +0800

    新增题目 1592.

---
+++ Sept2022/RearrangeSpacesBetweenWords.cs
@@ -0,0 +1,43 @@
+﻿// https://leetcode.cn/problems/rearrange-spaces-between-words/
+
+using CommonStructure;
+using System.Text;
+
+namespace Sept2022 {
+    public class RearrangeSpacesBetweenWords : ITestable {
+        public void RunTest() {
+            string[] tests = new string[] {
+                "  this   is  a sentence ",
+                " practice   makes   perfect",
+                "hello   world",
+                "  walks  udp package   into  bar a",
+                "a",
+                "  hello"
+            };
+            Solution solution = new();
+            foreach (string test in tests)
+                Console.WriteLine($"\"{solution.ReorderSpaces(test)}\"");
+        }
+        public class Solution {
+            public string ReorderSpaces(string text) {
+                string[] strings = text.Split(' ', 
+                    StringSplitOptions.TrimEntries | 
+                    StringSplitOptions.RemoveEmptyEntries);
+                if (strings.Length == 1) 
+                    return strings[0] 
+                        + new string(' ', text.Length - strings[0].Length);
+                int totalLength = 0;
+                Array.ForEach(strings, x => { totalLength += x.Length; });
+                int sepCount = (text.Length - totalLength) / (strings.Length - 1);
+                int append = (text.Length - totalLength) % (strings.Length - 1);
+                var sep = new string(' ', sepCount);
+                var end = new string(' ', append);
+                var ans = new StringBuilder(strings[0]);
+                for (int i = 1; i < strings.Length; ++i)
+                    ans.Append(sep + strings[i]);
+                ans.Append(end);
+                return ans.ToString();
+            }
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -4,7 +4,7 @@ using Sept2022;
 namespace Test {
     internal class Program {
         static void Main() {
-            ITestable test = new CountUniqueCharactersOfAllSubstringsOfAGivenString();
+            ITestable test = new RearrangeSpacesBetweenWords();
             test.RunTest();
         }
     }
