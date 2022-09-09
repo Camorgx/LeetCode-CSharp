3cb98ba2fd1fe4b941602cf735edfc69f5014e76
Bill <1821365384@qq.com>
2022-09-09 09:56:42 +0800

    新增题目 1598.

---
+++ Sept2022/CrawlerLogFolder.cs
@@ -0,0 +1,30 @@
+﻿// https://leetcode.cn/problems/crawler-log-folder/
+
+using CommonStructure;
+
+namespace Sept2022 {
+    public class CrawlerLogFolder : ITestable {
+        public void RunTest() {
+            string[][] tests = new string[][] {
+                new string[] { "d1/", "d2/", "../", "d21/", "./" },
+                new string[] { "d1/", "d2/", "./", "d3/", "../", "d31/" },
+                new string[] { "d1/", "../", "../", "../" },
+                new string[] { "./", "../", "./" }
+            };
+            Solution solution = new();
+            foreach (var test in tests)
+                Console.WriteLine(solution.MinOperations(test));
+        }
+        public class Solution {
+            public int MinOperations(string[] logs) {
+                int ans = 0;
+                foreach (string log in logs) {
+                    if (log == "./") continue;
+                    else if (log == "../") ans = ans > 0 ? ans - 1 : 0;
+                    else ++ans;
+                }
+                return ans;
+            }
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -4,7 +4,7 @@ using Sept2022;
 namespace Test {
     internal class Program {
         static void Main() {
-            ITestable test = new BeautifulArrangementII();
+            ITestable test = new CrawlerLogFolder();
             test.RunTest();
         }
     }
