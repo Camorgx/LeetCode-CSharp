c5398ca94950097629165256bcccb8ab6dab172e
曹高翔 <cgx@mail.ustc.edu.cn>
2022-09-11 00:05:54 +0800

    新增题目 669.

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,7 +4,7 @@ using Sept2022;
 namespace Test {
     internal class Program {
         static void Main() {
-            ITestable test = new BinaryPrefixDivisibleBy5();
+            ITestable test = new TrimABinarySearchTree();
             test.RunTest();
         }
     }
