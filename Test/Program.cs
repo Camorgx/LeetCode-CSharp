b2024fa530e678bef5babba57792f37020f79a21
曹高翔 <cgx@mail.ustc.edu.cn>
2022-09-13 10:25:28 +0800

    上传题目 670.

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,7 +4,7 @@ using Sept2022;
 namespace Test {
     internal class Program {
         static void Main() {
-            ITestable test = new SpecialArrayWithXElementsGreaterThanOrEqualX();
+            ITestable test = new MaximumSwap();
             test.RunTest();
         }
     }
