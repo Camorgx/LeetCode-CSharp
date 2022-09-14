a316c6c93eac708780cb12e8321d333df0369859
Bill <1821365384@qq.com>
2022-09-14 09:52:52 +0800

    新增题目 1619.

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,7 +4,7 @@ using Sept2022;
 namespace Test {
     internal class Program {
         static void Main() {
-            ITestable test = new CrawlerLogFolder();
+            ITestable test = new MeanOfArrayAfterRemovingSomeElements();
             test.RunTest();
         }
     }
