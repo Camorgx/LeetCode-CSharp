7e89c050d24b913c2a3fefc05f8895eb3f748ad9
Bill <1821365384@qq.com>
2022-09-15 10:43:38 +0800

    新增题目 672.

---
+++ Sept2022/BulbSwitcherII.cs
@@ -0,0 +1,24 @@
+﻿using CommonStructure;
+
+namespace Sept2022 {
+    public class BulbSwitcherII : ITestable {
+        public void RunTest() {
+            var tests = new (int n, int presses)[] {
+                (1, 1), (2, 1), (3, 1)
+            };
+            Solution solution = new();
+            foreach (var test in tests)
+                Console.WriteLine(solution.FlipLights(test.n, test.presses));
+        }
+        public class Solution {
+            public int FlipLights(int n, int presses) {
+                if (presses == 0) return 1;
+                return n switch {
+                    1 => 2,
+                    2 => presses == 1 ? 3 : 4,
+                    _ => presses == 1 ? 4 : (presses == 2 ? 7 : 8)
+                };
+            }
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -4,7 +4,7 @@ using Sept2022;
 namespace Test {
     internal class Program {
         static void Main() {
-            ITestable test = new MeanOfArrayAfterRemovingSomeElements();
+            ITestable test = new BulbSwitcherII();
             test.RunTest();
         }
     }
