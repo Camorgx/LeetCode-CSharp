a4a052dc2b02ef81d8474fd0cdd0b1d0e1e858b5
星河 <1821365384@qq.com>
2022-09-20 20:16:49 +0800

    新增题目 698.

---
+++ Sept2022/PartitionToKEqualSumSubsets.cs
@@ -0,0 +1,14 @@
+﻿using CommonStructure;
+
+namespace Sept2022 {
+    public class PartitionToKEqualSumSubsets : ITestable {
+        public void RunTest() {
+
+        }
+    }
+    public class Solution {
+        public bool CanPartitionKSubsets(int[] nums, int k) {
+
+        }
+    }
+}
