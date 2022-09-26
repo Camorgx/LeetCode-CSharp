0032413317241c92a19935de2afc17387588b8e5
星河 <1821365384@qq.com>
2022-09-26 11:00:28 +0800

    新增题目 面试题 17.19.

---
+++ Sept2022/MissingTwoLcci.cs
@@ -0,0 +1,34 @@
+﻿// https://leetcode.cn/problems/missing-two-lcci/
+
+using CommonStructure;
+
+namespace Sept2022 {
+    public class MissingTwoLcci : ITestable {
+        public void RunTest() {
+            var tests = new int[][] {
+                new int[] { 1 },
+                new int[] { 2, 3 }
+            };
+            var solution = new Solution();
+            foreach (var test in tests) {
+                var res = solution.MissingTwo(test);
+                foreach (var item in res)
+                    Console.Write($"{item} ");
+                Console.WriteLine();
+            }
+        }
+        public class Solution {
+            public int[] MissingTwo(int[] nums) {
+                int n = nums.Length + 2;
+                int ansSum = n * (n + 1) / 2;
+                Array.ForEach(nums, x => { ansSum -= x; });
+                int mid = ansSum / 2;
+                int ans = mid * (mid + 1) / 2;
+                Array.ForEach(nums, x => {
+                    if (x <= mid) ans -= x;
+                });
+                return new int[] { ans, ansSum - ans };
+            }
+        }
+    }
+}
