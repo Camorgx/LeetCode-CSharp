5d3b29ce158169497d3d5ec0bdf7e3c50d77174b
星河 <1821365384@qq.com>
2022-09-29 15:26:57 +0800

    新增题目 面试题 01.09.

---
+++ Sept2022/StringRotationLcci.cs
@@ -0,0 +1,23 @@
+﻿// https://leetcode.cn/problems/string-rotation-lcci/
+
+using CommonStructure;
+
+namespace Sept2022 {
+    public class StringRotationLcci : ITestable {
+        public void RunTest() {
+            var tests = new (string, string)[] {
+                ("waterbottle", "erbottlewat"),
+                ("aa", "aba")
+            };
+            var solution = new Solution();
+            foreach (var (s1, s2) in tests)
+                Console.WriteLine(solution.IsFlipedString(s1, s2));
+        }
+        public class Solution {
+            public bool IsFlipedString(string s1, string s2) {
+                return s1.Length == s2.Length
+                    && (s1 + s1).Contains(s2);
+            }
+        }
+    }
+}
