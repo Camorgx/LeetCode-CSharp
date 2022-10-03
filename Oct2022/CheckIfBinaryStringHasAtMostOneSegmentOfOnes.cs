20f635828b5fabbc21180012e92bc6cc17deaa8c
星河 <1821365384@qq.com>
2022-10-03 22:25:43 +0800

    新增题目 1784.

---
+++ Oct2022/CheckIfBinaryStringHasAtMostOneSegmentOfOnes.cs
@@ -0,0 +1,22 @@
+﻿// https://leetcode.cn/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+
+using CommonStructure;
+
+namespace Oct2022 {
+    public class CheckIfBinaryStringHasAtMostOneSegmentOfOnes
+        : ITestable {
+        public void RunTest() {
+            var tests = new string[] {
+                "1001",
+                "110"
+            };
+            var solution = new Solution();
+            foreach (var test in tests)
+                Console.WriteLine(solution.CheckOnesSegment(test));
+        }
+        public class Solution {
+            public bool CheckOnesSegment(string s)
+                => !s.Contains("01");
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -4,7 +4,7 @@ using Oct2022;
 namespace Test {
     internal class Program {
         static void Main() {
-            ITestable test = new SwapAdjacentInLRString();
+            ITestable test = new CheckIfBinaryStringHasAtMostOneSegmentOfOnes();
             test.RunTest();
         }
     }
