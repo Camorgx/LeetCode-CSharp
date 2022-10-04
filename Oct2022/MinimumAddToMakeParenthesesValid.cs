2db293989dab8eb0db9ba3e929c1674388e7288b
星河 <1821365384@qq.com>
2022-10-04 22:40:38 +0800

    新增题目 921.

---
+++ Oct2022/MinimumAddToMakeParenthesesValid.cs
@@ -0,0 +1,28 @@
+﻿// https://leetcode.cn/problems/minimum-add-to-make-parentheses-valid/
+
+using CommonStructure;
+
+namespace Oct2022 {
+    public class MinimumAddToMakeParenthesesValid : ITestable {
+        public void RunTest() {
+            var tests = new string[] {
+                "())",
+                "((("
+            };
+            var solution = new Solution();
+            foreach (var test in tests) 
+                Console.WriteLine(solution.MinAddToMakeValid(test));
+        }
+        public class Solution {
+            public int MinAddToMakeValid(string s) {
+                int ans = 0, cnt = 0;
+                foreach (char c in s) {
+                    if (c == '(') ++cnt;
+                    else if (cnt > 0) --cnt;
+                    else ++ans;
+                }
+                return ans + cnt;
+            }
+        }
+    }
+}
