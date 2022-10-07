2df562e4b5cb8851b03e825ab7b72c700b7d58bb
星河 <1821365384@qq.com>
2022-10-07 22:13:18 +0800

    新增题目 1800.

---
+++ Oct2022/MaximumAscendingSubarraySum.cs
@@ -0,0 +1,31 @@
+﻿// https://leetcode.cn/problems/maximum-ascending-subarray-sum/
+
+using CommonStructure;
+
+namespace Oct2022 {
+    public class MaximumAscendingSubarraySum : ITestable {
+        public void RunTest() {
+            var tests = new int[][] {
+                new int[] { 0, 20, 30, 5, 10, 50 },
+                new int[] { 10, 20, 30, 40, 50 },
+                new int[] { 12, 17, 15, 13, 10, 11, 12 },
+                new int[] { 100, 10, 1 }
+            };
+            var solution = new Solution();
+            foreach (var test in tests)
+                Console.WriteLine(solution.MaxAscendingSum(test));
+        }
+        public class Solution {
+            public int MaxAscendingSum(int[] nums) {
+                int curSum = nums[0], ans = nums[0];
+                for (int i = 1; i < nums.Length; ++i) {
+                    if (nums[i] > nums[i - 1])
+                        curSum += nums[i];
+                    else curSum = nums[i];
+                    ans = Math.Max(ans, curSum);
+                }
+                return ans;
+            }
+        }
+    }
+}
