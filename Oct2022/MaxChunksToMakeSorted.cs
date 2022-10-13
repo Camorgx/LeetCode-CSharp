7ad74b92dd90556f9101aa72a34b9792effe87d7
星河 <1821365384@qq.com>
2022-10-13 21:48:54 +0800

    新增题目 769。

---
+++ Oct2022/MaxChunksToMakeSorted.cs
@@ -0,0 +1,27 @@
+﻿// https://leetcode.cn/problems/max-chunks-to-make-sorted/
+
+using CommonStructure;
+
+namespace Oct2022 {
+    public class MaxChunksToMakeSorted : ITestable {
+        public void RunTest() {
+            var tests = new int[][] {
+                new int[] { 4, 3, 2, 1, 0 },
+                new int[] { 1, 0, 2, 3, 4 }
+            };
+            var solution = new Solution();
+            foreach (var test in tests)
+                Console.WriteLine(solution.MaxChunksToSorted(test));
+        }
+        public class Solution {
+            public int MaxChunksToSorted(int[] arr) {
+                int m = 0, res = 0;
+                for (int i = 0; i < arr.Length; i++) {
+                    m = Math.Max(m, arr[i]);
+                    if (m == i) ++res;
+                }
+                return res;
+            }
+        }
+    }
+}
